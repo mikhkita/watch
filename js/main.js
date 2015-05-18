58d137b4787a450802eaedaac1865b32d7d1acac
Михаил <beatbox787@gmail.com>
2015-05-18 14:23:48 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -22,25 +22,54 @@ $(document).ready(function(){
         xparallax: "50%"
     }); 
 
-    // var myPlace = new google.maps.LatLng(59.889371, 30.405350);
-    // var myOptions = {
-    //     zoom: 16,
-    //     center: myPlace,
-    //     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    //     disableDefaultUI: true,
-    //     scrollwheel: false,
-    //     zoomControl: true,
-    //     zoomControlOptions: {
-    //         style: google.maps.ZoomControlStyle.LARGE,
-    //         position: google.maps.ControlPosition.LEFT_CENTER
-    //     }
-    // }
-    // var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+    var myPlace = new google.maps.LatLng(59.889371, 30.400350);
+    var myOptions = {
+        zoom: 16,
+        center: myPlace,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            position: google.maps.ControlPosition.LEFT_CENTER
+        }
+    }
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
 
-    // var marker = new google.maps.Marker({
-    //     position: new google.maps.LatLng(59.889371, 30.405350),
-    //     map: map,
-    //     title: "Часы"
-    // });
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(59.889371, 30.405350),
+        map: map,
+        title: "Часы"
+    });
+
+    custom["monitor"] = function(){
+        var nowY = 0,
+            step = 388,
+            slides = 10,
+            timer,
+            dur = 50;
+
+        $(".b-monitor").css({
+            "opacity" : 1
+        });
+
+        timer = setTimeout(function run(){
+            nowY += step;
+            if( nowY <= step*(slides-1) ){
+                $(".b-monitor").css({
+                    "background-position" : "50% -"+nowY+"px"
+                });
+                setTimeout(run,dur);
+            }
+        },500);
+    }
+
+    $(".b-4-animation li").hover(function(){
+        document.getElementById($(this).attr("data-svg")).beginElement();
+        console.log("over");
+    },function(){
+        console.log("out");
+    });
 
 });
